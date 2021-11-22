ba5c586781eb32efeb139cc008169bfb169b24db
jdpy19 <jake.ditslear@gmail.com>
2021-11-21 19:55:07 -0500

    Style changes.

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <h1>Enodo Challenge</h1>
-    <Search />
-    <Table />
+  <div class="app">
+    <div class="header">
+      <h1>Enodo Challenge</h1>
+    </div>
+    <div>
+      <Table />
+    </div>
   </div>
 </template>
 
@@ -19,12 +22,19 @@ export default {
 </script>
 
 <style lang="scss">
+html {
+  background-color: $primary;
+}
+h1 {
+  color: $secondary;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: $primary;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
